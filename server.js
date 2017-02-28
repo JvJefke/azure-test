ea9bf29fcbce4e35abb9e217ef8e466a085e77b8
Jeroen Valcke <valcke_jeroen@hotmail.com>
2017-02-28 20:25:42 +0100

    use stream for index.html

--- server.js
+++ server.js
@@ -6,13 +6,8 @@ app.use("/api", function(req, res, next) {
 });
 
 app.use("*", function(req, res, next) {
-    fs.readFile('./index.html', function(err, result) {
-        if (!err) {
-            return res.status(200).send(result);
-        }
-
-        return res.status(400).json(err);
-    });
+    res.set("Content-Type", "text/html");
+    fs.createReadStream("./index.html").pipe(res);
 });
 
 app.listen(process.env.PORT || 4000, function() {
