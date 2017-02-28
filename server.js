e757178cd075a98293caf31d63b8a4bbcae10ee9
Jeroen Valcke <valcke_jeroen@hotmail.com>
2017-02-28 20:20:58 +0100

    custom sendFile

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var app = require("express")();
+var fs = require('fs');
 
 app.use("/api", function(req, res, next) {
     res.status(200).json({ "type": "api" });
 });
 
 app.use("*", function(req, res, next) {
-    res.status(200).sendFile("./index.html");
+    fs.readFile('./index.html', function(err, result) {
+        if (!err) {
+            return res.status(200).send(result);
+        }
+
+        return res.status(400).json(err);
+    });
 });
 
 app.listen(process.env.PORT || 4000, function() {
